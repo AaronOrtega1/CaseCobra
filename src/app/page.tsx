307d46515e77df3141042c58e863d04f5287ff33
AaronOrtega1 <aaron.ortega15@outlook.es>
2024-06-24 09:35:48 -0600

    new NavBar comp, authentication working using kinde and start of routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,10 @@ export default function Home() {
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
-            <div className="realtive mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                {/* gradiant -> not to my taste */}
+                {/* <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28" /> */}
                 <img src="/snake-1.png" className="w-full" alt="snake-1" />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
@@ -92,7 +94,7 @@ export default function Home() {
                 className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
               />
               <img src="/line.png" alt="" className="absolute w-20 -left-6 -bottom-6 select-none" />
-              <Phone />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { cn } from "@/lib/utils";
 import { HTMLAttributes } from "react";
 
@@ -7,7 +8,19 @@ interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
-  return <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)}></div>;
+  return (
+    <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)} {...props}>
+      <img
+        src={dark ? "/phone-template-dark-edges.png" : "/phone-template-white-edges.png"}
+        alt="phone image"
+        className="pointer-events-none z-50 select-none"
+      />
+
+      <div className="absolute -z-10 inset-0">
+        <img src="{imgSrc}" alt="ooooooooverlaying phone img" className="object-cover" />
+      </div>
+    </div>
+  );
 };
 
 export default Phone;
